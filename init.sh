7f705226aafae31dfbca9aa623c1e139d6e9308a
Mephis Pheies <mephistommm@gmail.com>
2016-08-05 18:58:40 +0800

    add init file

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This file init create the base directory of funs
+# 
+# Author: Mephis Pheies <mephistommm@gmail.com>
+# Begin ---
+
+mkdir -p logs/nginx  logs/supervisor logs/uwsgi
+
+echo "Funs init sucess!"
+echo "If you has readied your python3 flask project,"
+echo "you should go to config uwsgi file in ./configs/uwsgi_conf.d/config.ini,"
+echo "then run:"
+echo "  docker-compose up -d"
+# End  ---
